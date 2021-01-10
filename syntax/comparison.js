90e540b50ffc0504768c15d9788555e5ed7f6a6a
kimchaerin04 <s20013@gsm.hs.kr>
2021-01-10 21:00:50 +0900

    node.js

---
+++ syntax/comparison.js
@@ -0,0 +1,6 @@
+console.log(1==1); //true 좌항과 우항이 같냐
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1<2); //true
+console.log(1===1); //true 정확하게 같냐
+console.log(1===2); //false
