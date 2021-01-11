60c3b8c4dbb803d1e66b0afae11d1c632c982a42
kimchaerin04 <s20013@gsm.hs.kr>
2021-01-12 02:06:00 +0900

    반복문

--- main.js
+++ main.js
@@ -5,11 +5,39 @@ var url = require('url');
 var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
+    var pathname = url.parse(_url, true).pathname;
 
-
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-        var template = `
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                var title = 'Welcom';
+                var description = 'Hello, Node.js';
+                var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                var title = queryData.id;
+                var template = `
       <!doctype html>
       <html>
       <head>
@@ -28,10 +56,14 @@ var app = http.createServer(function(request, response) {
       </body>
       </html>
       `;
-        response.writeHead(200);
-        response.end(template);
-    })
-
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
 
 });
 app.listen(3000);
